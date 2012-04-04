6da33f508634d8f7eb260e2904c1c10a77ce3a9f
Craig Quiter <cquiter@gmail.com>
2012-04-04 14:45:37 -0700

    added example NDB model

--- models.py
+++ models.py
@@ -1,1 +1,13 @@
 from google.appengine.ext import ndb
+
+#class Post(ndb.Model):
+#  author = ndb.StringProperty()
+#  time = ndb.DateTimeProperty(auto_now_add=True)
+#  title = ndb.StringProperty()
+#  description = ndb.TextProperty(default='')
+#  categories = ndb.StringProperty(repeated=True)
+#  last_modified = ndb.DateTimeProperty(auto_now=True)
+#
+#  @property
+#  def agoTime(self):
+#    return agoTime(self.time)
